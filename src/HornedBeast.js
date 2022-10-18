54e5d87cd7d7be39302d3f4f8cbbca8af3f843da
Trace Dugar <tracedugar89@gmail.com>
2022-10-18 16:18:06 -0700

    Imported and integrated horned beast data.

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,9 +4,9 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <article>
-        <img src={this.props.url} alt=""/>
-        <h2>{this.props.name}</h2>
-        <p>{this.props.about}</p>
+        <img src={this.props.image_url} alt={this.props.keyword}/>
+        <h2>{this.props.title}</h2>
+        <p>{this.props.description}</p>
       </article>
     )
   }
